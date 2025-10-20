471b4d16b2c2d942412956460becf821a8fd3620
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 13:01:23 +0100

    Update L01_HelloWorld.lean

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -6,13 +6,15 @@ Level 1
 
 Title "Hello World"
 
-Introduction "We are going to begin by looking at the usage of two of our exams to expand them|
-! The two axioms are  add_zero: ∀ a :  F, a + 0 = a and add_comm : ∀ a b :  F , a + b = b + a" 
+Introduction "We are going to begin by looking at the usage of two of our exams to expand them
+! The two axioms are add_zero: ∀ a :  F, a + 0 = a and add_comm : ∀ a b :  F , a + b = b + a.
+Using these two axioms we want to expand upon add_zero to be in a more complete form." 
 
 variable {F : Type*} [MyField F]
 
-Statement (x y z : F) (h : x = z) : x * y = z * y := by --ok take it from here charlie
-  rw [h]
+Statement (x: F): a+0=0+a:= by --ok take it from here charlie
+  rw [MyField.add_zero a, MyField.add_comm a 0, MyField.add_zero 0]
+
 
 Conclusion "This last message appears if the level is solved."
 
