825a9baf92715658935f022737845463b22b9e7e
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 11:35:03 +0100

    Update DemoWorld.lean

--- Game/Levels/DemoWorld.lean
+++ Game/Levels/DemoWorld.lean
@@ -4,5 +4,10 @@ World "DemoWorld"
 Title "Demo World"
 
 Introduction "
-This introduction is shown before one enters level 1 of the demo world. Use markdown.
+To begin exploring Fields, we must start with what they are! A field is a set 
+F together with two binary operations,+: F ⨯ F → F and ∘: F ⨯ F → F such that our Field axioms hold.
+To begin with, we will simply state our axioms, but throughout this world we will explore them more,
+so do not worry if they apear daunting. 
+Informaly, we can view Fields as a set which we can perform adition and multiplication on, with 
+inverses. But this will be formaly defined too!
 "
