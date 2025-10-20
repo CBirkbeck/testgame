7f20bd41b384f1615f064839bec6b268d6d3ccfd
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 12:53:37 +0100

    Update L01_HelloWorld.lean

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -6,12 +6,8 @@ Level 1
 
 Title "Hello World"
 
-Introduction "To begin exploring Fields, we must start with what they are! A field is a set 
-F together with two binary operations,+: F ⨯ F → F and ∘: F ⨯ F → F such that our Field axioms hold.
-To begin with, we will simply state our axioms, but throughout this world we will explore them more,
-so do not worry if they apear daunting. 
-Informaly, we can view Fields as a set which we can perform adition and multiplication on, with 
-inverses. But this will be formaly defined too!" 
+Introduction "We are going to begin by looking at the usage of two of our exams to expand them|
+! The two axioms are  add_zero: ∀ a :  F, a + 0 = a and add_comm : ∀ a b :  F , a + b = b + a" 
 
 variable {F : Type*} [MyField F]
 
