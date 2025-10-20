7fbf8d36196b0ea27c0e9913e3ce84cdd3ae33ef
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 12:40:36 +0100

    Update L01_HelloWorld.lean

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -6,20 +6,22 @@ Level 1
 
 Title "Hello World"
 
-Introduction """In this level we are going to begin looking at one of our axioms, the add_zero axiom: ∀ a ∈ F ∃ 0 ∈ F s.t 0 + a = a.
-We want to extend it to ∀ a ∈ F ∃ a ∈ F s.t a + 0 = a = 0 + a.""" 
+Introduction "To begin exploring Fields, we must start with what they are! A field is a set 
+F together with two binary operations,+: F ⨯ F → F and ∘: F ⨯ F → F such that our Field axioms hold.
+To begin with, we will simply state our axioms, but throughout this world we will explore them more,
+so do not worry if they apear daunting. 
+Informaly, we can view Fields as a set which we can perform adition and multiplication on, with 
+inverses. But this will be formaly defined too!" 
 
 variable {F : Type*} [MyField F]
 
-Statement (a : F) : a + 0 = 0 + a := by
-  rw [MyField.add_zero a, MyField.add_comm a 0, MyField.add_zero 0]
+Statement (x y z : F) (h : x = z) : x * y = z * y := by --ok take it from here charlie
+  rw [h]
 
 Conclusion "This last message appears if the level is solved."
 
 /- Use these commands to add items to the game's inventory. -/
 
-NewTactic rw
-NewTactic rfl
+NewTactic rw rfl
 -- NewTheorem Nat.add_comm Nat.add_assoc
 -- NewDefinition Nat Add Eq
-
