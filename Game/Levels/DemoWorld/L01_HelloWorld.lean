2cadc56a6c35a68a2614832a514d6d2264ecd7d7
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 14:14:52 +0100

    Update L01_HelloWorld.lean

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -1,31 +1,27 @@
 import Game.Metadata
-import Game.Levels.Definitions -- this should import Game.Defs.MyField etc.
-open Game.Defs MyFieldLemmas
-
-/- Inventory: declare tactics & theorems BEFORE worlds -/
-NewTactic rw rfl
-TacticDoc rw "The `rw` tactic replaces terms using known equalities."
-/- If the game requires theorems to be unlocked, list them: -/
-NewTheorem Game.Defs.MyField.add_zero Game.Defs.MyField.add_comm
+import Game.Levels.Definitions
 
 World "DemoWorld"
 Level 1
 
 Title "Hello World"
+TacticDoc rw
+Introduction "We are going to begin by looking at the usage of two of our exams to expand them
+! The two axioms are add_zero: ∀ a :  F, a + 0 = a and add_comm : ∀ a b :  F , a + b = b + a.
+Using these two axioms we want to expand upon add_zero to be in a more complete" 
 
-Introduction "We are going to begin by looking at the usage of two axioms...
-The two axioms are add_zero: ∀ a : F, a + 0 = a and add_comm : ∀ a b : F, a + b = b + a."
+variable {F : Type*} [MyField F]
 
-variable {F : Type*} [Game.Defs.MyField F]
-
-Statement (x : F) : x + 0 = 0 + x := by
-  -- rewrite in the order that matches the goal
-  rw [Game.Defs.MyField.add_zero x]
-  rw [Game.Defs.MyField.add_comm x 0]
-  rw [Game.Defs.MyField.add_zero 0]
+Statement (x: F): x + 0 = 0 + x := by --ok take it from here charlie
+  rw [MyField.add_zero x, MyField.add_comm x 0, MyField.add_zero 0]
 
 Conclusion "This last message appears if the level is solved."
 
+/- Use these commands to add items to the game's inventory. -/
+
+NewTactic rw rfl
+-- NewTheorem Nat.add_comm Nat.add_assoc
+-- NewDefinition Nat Add Eq
 
 
 
