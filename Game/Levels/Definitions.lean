29f58b71397e7ecc650b027ed020ca79c69846cf
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 14:11:06 +0100

    Update Definitions.lean

--- Game/Levels/Definitions.lean
+++ Game/Levels/Definitions.lean
@@ -1,23 +1,24 @@
 import Mathlib
 
+namespace Game.Defs
 
 class MyField (F : Type*) extends Add F, Mul F, Zero F, One F where
-  add_zero : ∀ a :  F, a + 0 = a
-  add_inv : ∀ a : F , ∃ b : F,  a + b = 0 ∧ b + a = 0
-  add_comm : ∀ a b :  F , a + b = b + a
-  one_mul: ∀ a : F , a * 1 = a
-  mul_inv: ∀ a : F , a ≠ 0 → ∃ b : F, a * b = 1 ∧ b * a = 1
-  mul_asoc: ∀ a b c : F, (a * b) * c = a * (b * c)
-  mul_comm: ∀ a b : F, a * b = b * a
-  dist: ∀ a b c : F, (a + b) * c =  (a * c) +  (b * c)
+  add_zero : ∀ a : F, a + 0 = a
+  add_inv : ∀ a : F, ∃ b : F, a + b = 0 ∧ b + a = 0
+  add_comm : ∀ a b : F, a + b = b + a
+  one_mul : ∀ a : F, a * 1 = a
+  mul_inv : ∀ a : F, a ≠ 0 → ∃ b : F, a * b = 1 ∧ b * a = 1
+  mul_assoc : ∀ a b c : F, (a * b) * c = a * (b * c)
+  mul_comm : ∀ a b : F, a * b = b * a
+  dist : ∀ a b c : F, (a + b) * c = (a * c) + (b * c)
 
 variable {F : Type*} [MyField F]
 
-namespace MyField
+namespace MyFieldLemmas
 
-lemma test (a b : F) : a + b = b + a := by
-  exact add_comm a b
+lemma add_comm' (a b : F) : a + b = b + a := by exact MyField.add_comm a b
+lemma add_zero' (a : F) : a + 0 = a := by exact MyField.add_zero a
 
-lemma zero_add (a : F) : 0 + a = a := by
-  rw [test 0 a]
-  apply add_zero
+end MyFieldLemmas
+
+end Game.Defs
