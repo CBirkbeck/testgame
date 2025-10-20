ad4a3e4ca4022d42c28241f2e6d3cbe35a7ffb40
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-20 12:35:03 +0100

    Update L01_HelloWorld.lean

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -6,18 +6,20 @@ Level 1
 
 Title "Hello World"
 
-Introduction """In this level we are going to begin looking at one of our axioms, the add_zero axiom: ∀ a ∈ F ∃ 0 ∈ F s.t 0+a=a. We want to extend it to 
-∀ a ∈ F ∃ a ∈ F s.t a+0=a=0+a"""
+Introduction """In this level we are going to begin looking at one of our axioms, the add_zero axiom: ∀ a ∈ F ∃ 0 ∈ F s.t 0 + a = a.
+We want to extend it to ∀ a ∈ F ∃ a ∈ F s.t a + 0 = a = 0 + a.""" 
 
 variable {F : Type*} [MyField F]
 
-Statement (a : F) : a+0 =0+a := by --ok take it from here charlie
+Statement (a : F) : a + 0 = 0 + a := by
   rw [MyField.add_zero a, MyField.add_comm a 0, MyField.add_zero 0]
 
 Conclusion "This last message appears if the level is solved."
 
 /- Use these commands to add items to the game's inventory. -/
 
-NewTactic rw rfl
+NewTactic rw
+NewTactic rfl
 -- NewTheorem Nat.add_comm Nat.add_assoc
 -- NewDefinition Nat Add Eq
+
