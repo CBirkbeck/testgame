27f0e982377261cb00321df611b7040b7ae81ff1
biscuitthepigy <biscuitthepig@gmail.com>
2025-10-21 14:15:11 +0100

    fixed levels

--- Game/Levels/DemoWorld.lean
+++ Game/Levels/DemoWorld.lean
@@ -4,10 +4,10 @@ World "DemoWorld"
 Title "Demo World"
 
 Introduction "
-To begin exploring Fields, we must start with what they are! A field is a set 
+To begin exploring Fields, we must start with what they are! A field is a set
 F together with two binary operations,+: F ⨯ F → F and ∘: F ⨯ F → F such that our Field axioms hold.
 To begin with, we will simply state our axioms, but throughout this world we will explore them more,
-so do not worry if they apear daunting. 
-Informaly, we can view Fields as a set which we can perform adition and multiplication on, with 
+so do not worry if they apear daunting.
+Informaly, we can view Fields as a set which we can perform adition and multiplication on, with
 inverses. But this will be formaly defined too!
 "
--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -5,23 +5,19 @@ World "DemoWorld"
 Level 1
 
 Title "Hello World"
-TacticDoc rw
+
 Introduction "We are going to begin by looking at the usage of two of our exams to expand them
 ! The two axioms are add_zero: ∀ a :  F, a + 0 = a and add_comm : ∀ a b :  F , a + b = b + a.
-Using these two axioms we want to expand upon add_zero to be in a more complete" 
+Using these two axioms we want to expand upon add_zero to be in a more complete"
 
 variable {F : Type*} [MyField F]
 
-Statement (x: F): x + 0 = 0 + x := by --ok take it from here charlie
-  rw [MyField.add_zero x, MyField.add_comm x 0, MyField.add_zero 0]
+Statement (x : F): x + 0 = 0 + x := by
+  rw [MyField.add_comm x 0]   -- rewrites goal to x + 0 = x
 
 Conclusion "This last message appears if the level is solved."
 
 /- Use these commands to add items to the game's inventory. -/
 
-NewTactic rw rfl
 -- NewTheorem Nat.add_comm Nat.add_assoc
 -- NewDefinition Nat Add Eq
-
-
-
