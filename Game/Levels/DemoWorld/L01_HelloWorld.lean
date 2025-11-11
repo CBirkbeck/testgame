5efc94ee3e078b298cf58bdb798542167690b91f
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2025-11-11 10:41:32 +0000

    save

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -8,16 +8,24 @@ Level 1
 Title "Exploring the identity and communativity"
 
 Introduction "We are going to begin by looking at the usage of two of our exams to expand them
-! The two axioms are add_zero: ∀ a :  F, a + 0 = a and add_comm : ∀ a b :  F , a + b = b + a.
+! The two axioms are add_zero: `∀ a :  F, a + 0 = a` and add_comm : ∀ a b :  F , a + b = b + a.
 Using these two axioms we want to expand upon add_zero to be in a more complete. What i mean by this
-is that ater this level we will not only be able to say that a+0=a, but that 0+a =a."
+is that ater this level we will not only be able to say that a + 0 = a, but that 0+a =a."
 
 variable {F : Type*} [MyField F]
 
-Statement (x : F): x + 0 = 0 + x := by
-  rw [MyField.add_comm x 0]   -- rewrites goal to x + 0 = x
+open MyField
 
-Conclusion "Congrtatulations! You have completed your first level
+/--
+Theorem stating that adding zero on the left leaves an element unchanged:
+`0 + a = a `.
+-/
+TheoremDoc MyField.zero_add as "zero_add" in "Addition"
+
+Statement MyField.zero_add (x : F): x + 0 = 0 + x := by
+  rw [add_comm x 0]   -- rewrites goal to x + 0 = x
+
+Conclusion "Congratulations! You have completed your first level
 and can now use the theorem zero_add, you shall see it in theorems on the
 next level."
 
@@ -37,6 +45,7 @@ Theorem stating that addition is commutative:
 `a + b = b + a`.
 -/
 TheoremDoc MyField.add_comm as "add_comm" in "Addition"
+
 
 
 NewTheorem MyField.add_zero MyField.add_comm
