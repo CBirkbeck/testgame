647f284d233e44e16091a9be4186d27655e44457
biscuitthepigy <biscuitthepig@gmail.com>
2026-03-27 12:33:24 +0000

    ordering

--- Game/Levels/SubSpaces.lean
+++ Game/Levels/SubSpaces.lean
@@ -2,6 +2,7 @@ import Game.Levels.SubSpaces.L01_SubSpaces
 import Game.Levels.SubSpaces.L02_SubSpaces
 import Game.Levels.SubSpaces.L03_SubSpaces
 import Game.Levels.SubSpaces.L04_SubSpaces
-Prerequisites "VectorSpaces"
+
 World "SubSpaces"
 Title "Sub Spaces"
+Prerequisites "VectorSpaces"
