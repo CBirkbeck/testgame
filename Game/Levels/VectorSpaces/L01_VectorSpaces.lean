7b32c7a7e8384384c90147d44568cbd5d12ebce8
biscuitthepigy <biscuitthepig@gmail.com>
2026-03-29 21:19:40 +0100

    changing theorem names

--- Game/Levels/VectorSpaces/L01_VectorSpaces.lean
+++ Game/Levels/VectorSpaces/L01_VectorSpaces.lean
@@ -13,34 +13,34 @@ variable {F V : Type*} [MyField F] [MyVectorSpace F V]
 /--
 Vector addition has a right identity: `v + 0 = v`.
 -/
-TheoremDoc MyVectorSpace.add_zero as "add_zero" in "VectorSpace"
+TheoremDoc MyVectorSpace.add_zero as "vec_add_zero" in "VectorSpace"
 
 /--
 Vector addition is commutative: `u + v = v + u`.
 -/
-TheoremDoc MyVectorSpace.add_comm as "add_comm" in "VectorSpace"
+TheoremDoc MyVectorSpace.add_comm as "vec_add_comm" in "VectorSpace"
 
 /--
 Every vector has an additive inverse: `∀ v, ∃ w, v + w = 0 ∧ w + v = 0`.
 -/
-TheoremDoc MyVectorSpace.add_inv as "add_inv" in "VectorSpace"
+TheoremDoc MyVectorSpace.add_inv as "vec_add_inv" in "VectorSpace"
 
 /--
 Vector addition is associative: `(u + v) + w = u + (v + w)`.
 -/
-TheoremDoc MyVectorSpace.add_asoc as "add_asoc" in "VectorSpace"
+TheoremDoc MyVectorSpace.add_asoc as "vec_add_asoc" in "VectorSpace"
 
 /--
 Scalar multiplication distributes over vector addition: `α • (v + w) = α • v + α • w`.
 -/
-TheoremDoc MyVectorSpace.smul_add as "smul_add" in "VectorSpace"
+TheoremDoc MyVectorSpace.smul_add as "vec_smul_add" in "VectorSpace"
 
 NewTheorem MyVectorSpace.add_zero MyVectorSpace.add_comm MyVectorSpace.add_inv MyVectorSpace.add_asoc MyVectorSpace.smul_add
 
 /--
 Vectors can be permuted in a sum of four: `v₁ + v₂ + v₃ + v₄ = v₂ + v₁ + v₄ + v₃`.
 -/
-TheoremDoc MyVectorSpace.add_perm4 as "add_perm4" in "VectorSpace"
+TheoremDoc MyVectorSpace.add_perm4 as "vec_add_perm4" in "VectorSpace"
 
 Statement MyVectorSpace.add_perm4 (v_1 v_2 v_3 v_4 : V) :
     v_1 + v_2 + v_3 + v_4 = v_2 + v_1 + v_4 + v_3 := by
