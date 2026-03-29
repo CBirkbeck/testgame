bafe29009efed7a5b802a3e121f068a6f37cf2f9
biscuitthepigy <biscuitthepig@gmail.com>
2026-03-29 20:59:11 +0100

    fixing where tactics are displayed in game

--- Game/Levels/VectorSpaces/L01_VectorSpaces.lean
+++ Game/Levels/VectorSpaces/L01_VectorSpaces.lean
@@ -13,34 +13,34 @@ variable {F V : Type*} [MyField F] [MyVectorSpace F V]
 /--
 Vector addition has a right identity: `v + 0 = v`.
 -/
-TheoremDoc MyVectorSpace.add_zero as "add_zero" in "MyVectorSpace"
+TheoremDoc MyVectorSpace.add_zero as "add_zero" in "VectorSpace"
 
 /--
 Vector addition is commutative: `u + v = v + u`.
 -/
-TheoremDoc MyVectorSpace.add_comm as "add_comm" in "MyVectorSpace"
+TheoremDoc MyVectorSpace.add_comm as "add_comm" in "VectorSpace"
 
 /--
 Every vector has an additive inverse: `∀ v, ∃ w, v + w = 0 ∧ w + v = 0`.
 -/
-TheoremDoc MyVectorSpace.add_inv as "add_inv" in "MyVectorSpace"
+TheoremDoc MyVectorSpace.add_inv as "add_inv" in "VectorSpace"
 
 /--
 Vector addition is associative: `(u + v) + w = u + (v + w)`.
 -/
-TheoremDoc MyVectorSpace.add_asoc as "add_asoc" in "MyVectorSpace"
+TheoremDoc MyVectorSpace.add_asoc as "add_asoc" in "VectorSpace"
 
 /--
 Scalar multiplication distributes over vector addition: `α • (v + w) = α • v + α • w`.
 -/
-TheoremDoc MyVectorSpace.smul_add as "smul_add" in "MyVectorSpace"
+TheoremDoc MyVectorSpace.smul_add as "smul_add" in "VectorSpace"
 
 NewTheorem MyVectorSpace.add_zero MyVectorSpace.add_comm MyVectorSpace.add_inv MyVectorSpace.add_asoc MyVectorSpace.smul_add
 
 /--
 Vectors can be permuted in a sum of four: `v₁ + v₂ + v₃ + v₄ = v₂ + v₁ + v₄ + v₃`.
 -/
-TheoremDoc MyVectorSpace.add_perm4 as "add_perm4" in "Addition"
+TheoremDoc MyVectorSpace.add_perm4 as "add_perm4" in "VectorSpace"
 
 Statement MyVectorSpace.add_perm4 (v_1 v_2 v_3 v_4 : V) :
     v_1 + v_2 + v_3 + v_4 = v_2 + v_1 + v_4 + v_3 := by
--- Game/Levels/VectorSpaces/L02_VectorSpaces.lean
+++ Game/Levels/VectorSpaces/L02_VectorSpaces.lean
@@ -15,7 +15,7 @@ variable {F V : Type*} [MyField F] [MyVectorSpace F V]
 Scalar multiplication distributes over a sum of four vectors:
 `α • (v₁ + v₂ + v₃ + v₄) = α•v₁ + α•v₂ + α•v₃ + α•v₄`.
 -/
-TheoremDoc MyVectorSpace.smul_add4 as "smul_add4" in "SMul"
+TheoremDoc MyVectorSpace.smul_add4 as "smul_add4" in "VectorSpace"
 
 Statement MyVectorSpace.smul_add4 (v_1 v_2 v_3 v_4 : V) (α : F) :
     α • (v_1 + v_2 + v_3 + v_4) = α • v_1 + α • v_2 + α • v_3 + α • v_4 := by
