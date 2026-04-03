ab051ad6cac75c0418fe482dde508445505e66c4
biscuitthepigy <biscuitthepig@gmail.com>
2026-04-03 10:56:58 +0100

    lin

--- Game/Levels/LinearCombinations.lean
+++ Game/Levels/LinearCombinations.lean
@@ -1,5 +1,5 @@
-import Game.Levels.DemoWorld.L01_LinearCombinations
-import Game.Levels.DemoWorld.L02_LinearCombinations
+import Game.Levels.LinearCombinations.L01_LinearCombinations
+import Game.Levels.LinearCombinations.L02_LinearCombinations
 
 World "LinearCombinations"
 Title "Linear combinations"
